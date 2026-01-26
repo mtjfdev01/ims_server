e1b613a3129c6a431301d3c3cace359eb42d8387
mtjf development <dev@mtjfoundation.org>
2026-01-26 14:45:14 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ import { seedUsers } from './users/seed/users.seed';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://ims-client-eight.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
